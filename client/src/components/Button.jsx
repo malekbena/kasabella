0a517be41cd29745ee30348fef66f74cbb98f29d
malekbena <malekb.benali@gmail.com>
2024-01-02 16:51:33 +0100

    dashboard css base

---
+++ client/src/components/Button.jsx
@@ -0,0 +1,7 @@
+const Button = ({ text, onClick, className }) => {
+    return (
+        <button className={`button ${className}`} onClick={onClick}>{text}</button>
+    )
+}
+
+export default Button
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,34 @@
+import { useState, useEffect, useContext } from "react"
+import { getData } from "../util"
+import Cards from "../components/Cards"
+import Button from "../components/Button"
+import { AuthContext } from "../context/AuthContext"
+
 const Dashboard = () => {
+    const [data, setData] = useState([])
+    const [isLoaded, setIsLoaded] = useState(false)
+
+    const {user} = useContext(AuthContext)
+
+    useEffect(() => {
+        getData('/accomodations').then((data) => {
+            setData(data.accomodations)
+            setIsLoaded(true)
+        })
+    }, []);
+
     return (
-        <div>
+        <div className="dashboard">
             <h1>Dashboard</h1>
+            <p>Bonjour {user.username}</p>
+            <div className="dashboard_titles">
+                <h2>Liste des logements</h2>
+                <Button text="Ajouter un logement" className="button__add" />
+            </div>
+            {
+                isLoaded && data &&
+                <Cards accomodations={data} />
+            }
         </div>
     )
 }
